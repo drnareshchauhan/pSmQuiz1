5eb7df3f630943dcd992c9e29c5ec4b0d0e3b47e
drnareshchauhan <drnareshchauhan@gmail.com>
2022-03-23 21:56:39 +0530

    psmQuiz

---
+++ src/public/readme.md
@@ -0,0 +1,5 @@
+- Rename the folder to public.
+- Create a production build of the frontend web app.
+- Place the files of the build in the 'public' folder.
+
+i have used my psmstudent@**\*** account for mongodb :to remember
---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const userRoute = require("./Routes/Users");
+const quizzesRoute = require("./Routes/Quizzes");
+
+// Hosting Frontend
+// Create a production build of the frontend and paste the files in the public folder
+app.use(express.static(path.join(__dirname, "/public/")));
+
+// Middleware
+app.use(express.json());
+app.use("/API/users", userRoute);
+app.use("/API/quizzes", quizzesRoute);
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+// Listening to APIs
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
+
+// app.listen(process.env.PORT || 8000, () =>
+// 	console.log('Listening on Port 8000')
+// )
+
+// app.listen(process.env.PORT || 3000, () => { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// 					   });
